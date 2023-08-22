8a27420c523dc07946814dd7909b858a0337a274
HenryDiaz05 <142328292+HenryDiaz05@users.noreply.github.com>
2023-08-22 09:39:13 -0500

    Add files via upload

---
+++ PrimerosProyectos/holaMundo/holamundo/sixthProgram.py
@@ -0,0 +1,12 @@
+# 2 potencia de 10, 1024
+print(2 ** 10)
+ 
+# 8 al cuadrado, 64
+print(8 ** 2)
+ 
+# 9*9*9, nueve al cubo o 729
+print(9 ** 3)
+ 
+# Podemos utilizar fraciones como potencia 
+# 4 a la 1/2 o raiz de 4, es 2
+print(4 ** 0.5)
