574bd5bbd0f965a2f33e8e50dd46dad0e425e2cb
HenryDiaz05 <142328292+HenryDiaz05@users.noreply.github.com>
2023-08-22 09:42:44 -0500

    Delete PrimerosProyectos/holaMundo directory

--- PrimerosProyectos/holaMundo/holamundo/fourthProgram.py
+++
@@ -1,5 +0,0 @@
-# Python maneja la jerarquia de operadores
-print(42 * 6 + 64 / 8) # = 260
-print((42 * 6) + (64 / 8)) # = 260
-print(5*18/36+6) # = 8.5
-print((5*18)/(36+6)) # = 2.1428
--- PrimerosProyectos/holaMundo/holamundo/seventhProgram.py
+++
@@ -1,9 +0,0 @@
-# Imprime 4 ya que es el residuo de la parte entera de un numero
-print(29 % 5)
- 
-# Imprime 1
-print(64 % 3)
- 
-
-# Imprime 0, ya que es una division exacta o no tiene parte decimal
-print(10 % 2)
