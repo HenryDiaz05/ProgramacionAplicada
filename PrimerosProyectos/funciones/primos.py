9c420126273b5e1ec31c63daee1f52e82037b80f
HenryDiaz05 <142328292+HenryDiaz05@users.noreply.github.com>
2023-08-29 10:03:47 -0500

    Add files via upload

---
+++ PrimerosProyectos/funciones/primos.py
@@ -0,0 +1,37 @@
+lista = []
+
+def intervalo():
+    n = int(input("ingrese el rango de numeros: "))
+    primo(n)
+    
+        
+def primo(n):
+    a = 2
+    primo = True
+    while (a < n):
+        for i in range(2, a):
+            if (a % i == 0):
+                primo = False
+        if (primo):
+            anexar(a)
+        else:
+            primo = True
+        a += 1
+    
+def anexar(a):
+    lista.append(a)
+    return lista
+
+def main():
+    intervalo()
+        
+
+if __name__ == '__main__':
+    main()
+    print(lista)
+
+
+
+
+
+        
